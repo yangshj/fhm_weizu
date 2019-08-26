4a823df328e5b487970ae8d423631cd6f28e08d2
shuaijun.yang <shuaijun.yang@ucarinc.com>
2019-08-26 21:12:20 +0800

    通过位置判断是否有权限

--- src/main/java/com/fh/listener/WebAppContextListener.java
+++ src/main/java/com/fh/listener/WebAppContextListener.java
@@ -3,9 +3,16 @@ package com.fh.listener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import com.mysql.jdbc.AbandonedConnectionCleanupThread;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.fh.util.Const;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 /**
  * 
 * 类名称：WebAppContextListener.java
@@ -18,7 +25,26 @@ public class WebAppContextListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
-		// TODO Auto-generated method stub
+		System.out.println("************** Shutting down! **************");
+		System.out.println("Destroying Context...");
+		System.out.println("Calling MySQL AbandonedConnectionCleanupThread checkedShutdown");
+		AbandonedConnectionCleanupThread.checkedShutdown();
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+			if (driver.getClass().getClassLoader() == cl) {
+				try {
+					System.out.println("Deregistering JDBC driver {}");
+					DriverManager.deregisterDriver(driver);
+				} catch (SQLException ex) {
+					System.out.println("Error deregistering JDBC driver {}");
+					ex.printStackTrace();
+				}
+			} else {
+				System.out.println("Not deregistering JDBC driver {} as it does not belong to this webapp's ClassLoader");
+			}
+		}
 	}
 
 	@Override
