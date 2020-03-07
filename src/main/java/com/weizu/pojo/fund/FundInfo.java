9eccf13953c1037e56cac3bbf72dbebec7c157e9
shuaijun.yang <wx_fcaf91b562ab4885bc66c8c4ae015d06@git.code.tencent.com>
2020-03-07 14:45:16 +0800

    提交工具类

---
+++ src/main/java/com/weizu/pojo/fund/FundInfo.java
@@ -0,0 +1,19 @@
+package com.weizu.pojo.fund;
+
+/**
+ * 基金信息类
+ * 对应http接口解析数据
+ */
+public class FundInfo {
+
+
+    private FundBean fundBean;
+
+
+    public FundBean getFundBean() {
+        return fundBean;
+    }
+    public void setFundBean(FundBean fundBean) {
+        this.fundBean = fundBean;
+    }
+}
--- src/main/java/com/weizu/util/UrlUtil.java
+++ src/main/java/com/weizu/util/UrlUtil.java
@@ -20,7 +20,7 @@ public class UrlUtil {
 	 * 向指定 URL 发送POST方法的请求
 	 * 
 	 * @param url 发送请求的 URL
-	 * @param param 请求参数
+	 * @param paramMap 请求参数
 	 * @return 所代表远程资源的响应结果
 	 */
 	public static String sendPost(String url, Map<String, ?> paramMap) {
@@ -55,6 +55,67 @@ public class UrlUtil {
 			out.print(param);
 			// flush输出流的缓冲
 			out.flush();
+			// 定义BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(
+					conn.getInputStream(), "UTF-8"));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		} finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+
+	/**
+	 * 向指定 URL 发送POST方法的请求
+	 *
+	 * @param url 发送请求的 URL
+	 * @param paramMap 请求参数
+	 * @return 所代表远程资源的响应结果
+	 */
+	public static String sendGet(String url, Map<String, ?> paramMap) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+
+		String param = "";
+		Iterator<String> it = paramMap.keySet().iterator();
+
+		while (it.hasNext()) {
+			String key = it.next();
+			param += key + "=" + paramMap.get(key) + "&";
+		}
+		// 连接后面拼接参数
+		url = url + "?"+ param;
+
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			conn.setConnectTimeout(6000);
+			conn.setReadTimeout(6000);
+			// 设置通用的请求属性
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("Accept-Charset", "utf-8");
+			conn.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			// 获取URLConnection对象对应的输出流
+			conn.connect();
 			// 定义BufferedReader输入流来读取URL的响应
 			in = new BufferedReader(new InputStreamReader(
 					conn.getInputStream(), "UTF-8"));
