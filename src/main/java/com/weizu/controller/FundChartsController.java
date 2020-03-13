7ab75457d4261e42d95c116eca5a3583993bce87
shuaijun.yang <wx_fcaf91b562ab4885bc66c8c4ae015d06@git.code.tencent.com>
2020-03-13 18:39:47 +0800

    报表

--- src/main/java/com/weizu/controller/FundChartsController.java
+++ src/main/java/com/weizu/controller/FundChartsController.java
@@ -2,11 +2,11 @@ package com.weizu.controller;
 
 import com.fh.controller.base.BaseController;
 import com.fh.entity.Page;
-import com.fh.util.AppUtil;
 import com.fh.util.Const;
-import com.fh.util.Jurisdiction;
 import com.fh.util.PageData;
 import com.weizu.pojo.fund.FundBean;
+import com.weizu.pojo.fund.FundNetWorthBean;
+import com.weizu.service.fund.FundNetWorthService;
 import com.weizu.service.fund.FundService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
@@ -14,19 +14,17 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 @RequestMapping(value = "/weizu/fundCharts")
 public class FundChartsController extends BaseController {
     String menuUrl = "weizu/fundCharts/list.do";
+    @Autowired
+    private FundNetWorthService fundNetWorthService;
     @Autowired
     private FundService fundService;
 
@@ -39,15 +37,36 @@ public class FundChartsController extends BaseController {
         PageData pd = new PageData();
         try{
             pd = this.getPageData();
-            String code = pd.getString("code");
-            if(null != code && !"".equals(code)){
-                code = code.trim();
-                pd.put("code", code);
+            List<FundBean> fundList = fundService.findFundByCondition(null);
+            String fundId = pd.getString("fundId");
+            FundNetWorthBean param = new FundNetWorthBean();
+            if(null != fundId && !"".equals(fundId)){
+                fundId = fundId.trim();
+            } else {
+                fundId = fundList.get(0).getId().toString();
             }
-            page.setPd(pd);
-            List<PageData>	userList = fundService.getAllFundListPage(page);	//列出所有列表
+            pd.put("fundId", fundId);
+            param.setFundId(Long.parseLong(fundId));
+            //列出所有列表
+            List<FundNetWorthBean>	itemList = fundNetWorthService.findAllFundNetWorthByCondition(param);
+            // 数据转换
+            List<List<String>> dataList = new ArrayList<>();
+            SimpleDateFormat format = new SimpleDateFormat("yyMMdd");
+            SimpleDateFormat format2 = new SimpleDateFormat("yyyyMMdd");
+            for(FundNetWorthBean item: itemList){
+                List<String> dataItemList =  new ArrayList<>();
+                Date worthDate = format.parse(item.getWorthDate());
+                dataItemList.add(format2.format(worthDate));
+                if(item.getNetWorth()!=null){
+                    dataItemList.add(item.getNetWorth().toString());
+                }else{
+                    continue;
+                }
+                dataList.add(dataItemList);
+            }
             mv.setViewName("weizu/fundCharts/list");
-            mv.addObject("userList", userList);
+            mv.addObject("dataList", dataList);
+            mv.addObject("fundList",fundList);
             mv.addObject("pd", pd);
             mv.addObject(Const.SESSION_QX,this.getHC());	//按钮权限
         } catch(Exception e){
